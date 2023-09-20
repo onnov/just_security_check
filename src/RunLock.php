86fa445138a84668f7b7f199dcb5fa11ff7b68e9
sv <oblnn@yandex.ru>
2023-09-21 00:43:12 +0300

    master dev

--- src/RunLock.php
+++ src/RunLock.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JautSecurityCheck\Console;
+namespace JustSecurityCheck\Console;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
